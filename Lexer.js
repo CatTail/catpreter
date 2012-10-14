b85075e9a3a56389c38b19655b476602ec7c6376
Chiyu Zhong <1606069+CatTail@users.noreply.github.com>
2012-10-14 16:43:01 +0800

    tokens

--- Lexer.js
+++ Lexer.js
@@ -1,7 +1,37 @@
-functino Lexer(source){
-	var _this = this;
-	var source = source;
-	var tokenStart = 0;
-	var tokenEnd = 0;
+var tokens = require('./tokens').tokens;
+var Cat = require('./cat').Cat;
+
+function Lexer(source){
+	var _this = this,
+	source = source,
+	tokenStart = 0,
+	tokenEnd = 0,
+	index = 0,
+	buffer = '';
+
+	_this.hasNext = function(){
+		for(var i=0;i<5;i++){
+			buffer += source[index];
+			index++;
+			var backtrack = undefined;
+			var matches = tokens;
+			while (matches.length!==0) {
+				backtrack = matches;
+				var tmp = [];
+				Cat.each(matches,function(reg){
+					reg.test(buffer) ? tmp.push(reg) : 0;
+					reg.lastIndex = 0;
+					debugger;
+				});
+				matches = tmp;
+				console.log(matches);
+			}
+			buffer = '';
+		}
+	};
+	_this.next = function(){
+	};
 
 }
+
+exports.Lexer = Lexer;
