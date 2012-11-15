39e0eee07a306ebb603f2843fcf5410071271371
cattail <cattail2012@gmail.com>
2012-11-15 16:56:40 +0800

    Add cat.js submodule

--- js/parser.js
+++ js/parser.js
@@ -1,9 +1,53 @@
-#! /usr/bin/env node
-
 var util = require('util');
-var grammar = require('./grammar.js').grammar;
 var Lexer = require('./lexer.js').Lexer;
-console.log(grammar);
+var grammar = require('./grammar.js').grammar;
+var cat = require('../lib/cat.js');
 
-// Eliminate left recursive
-// Extract left common factor
+/**
+ * @constructor
+ */
+var Parser = function(lexer,grammar){
+  this.lexer = lexer;
+  this.grammar = grammar;  
+
+  this.extFactor();
+  this.eliRecur();
+};
+
+/**
+ * Eliminate left recursive
+ */
+Parser.prototype.eliRecur = function(){
+/*
+  for (var head in this.grammar) {
+    this.grammar[head].forEach(function(body){
+      if (head === body) {
+        var body = this.grammar[head][idx];
+      }
+    });
+  }
+*/
+};
+
+/**
+ * Extract left common factor.
+ */
+Parser.prototype.extFactor = function(){
+  for (var head in this.grammar) {
+    this.widthFirst(head);
+  }
+};
+
+/**
+ * Using width-first technique to extract factor.
+ * @param {String} head Production head
+ * @returns {Array} Array of indexes have the same common factor
+ */
+Parser.prototype.widthFirst = function(head){
+  var level1 ; 
+  for (var body in this.grammar[head]) {
+    level1.push(body);
+  }
+};
+
+new Parser(new Lexer(), grammar);
