90db0471aaf68783c97cd78ecf640184c3aa85ee
cattail <cattail2012@gmail.com>
2013-01-04 16:59:43 +0800

    start to crete DnD

--- js/sea-modules/catpreter/catpreter.js
+++ js/sea-modules/catpreter/catpreter.js
@@ -378,7 +378,7 @@ Catpreter.Parser = (function () {
 
   generator.warn = function warn () {
     var args = Array.prototype.slice.call(arguments,0);
-    Catpreter.print.call(null,args.join(""));
+    Catpreter.print.call(console, args.join(""));
   };
 
   generator.error = function error (msg) {
@@ -1023,14 +1023,14 @@ Catpreter.Parser = (function () {
     opt = typal.mix.call({}, this.options, opt);
     var moduleName = opt.moduleName || "parser";
     var out = this.generateModule(opt)
-    + "\nif (typeof require !== 'undefined' && typeof exports !== 'undefined') {"
-    + "\nexports.parser = "+moduleName+";"
-    + "\nexports.Parser = "+moduleName+".Parser;"
-    + "\nexports.parse = function () { return "+moduleName+".parse.apply("+moduleName+", arguments); };"
-    + "\nexports.main = "+ String(opt.moduleMain || commonjsMain) + ";"
-    + "\nif (typeof module !== 'undefined' && require.main === module) {\n"
-    + "  exports.main(typeof process !== 'undefined' ? process.argv.slice(1) : require(\"system\").args);\n}"
-    + "\n}"
+//    + "\nif (typeof require !== 'undefined' && typeof exports !== 'undefined') {"
+//    + "\nexports.parser = "+moduleName+";"
+//    + "\nexports.Parser = "+moduleName+".Parser;"
+//    + "\nexports.parse = function () { return "+moduleName+".parse.apply("+moduleName+", arguments); };"
+//    + "\nexports.main = "+ String(opt.moduleMain || commonjsMain) + ";"
+//    + "\nif (typeof module !== 'undefined' && require.main === module) {\n"
+//    + "  exports.main(typeof process !== 'undefined' ? process.argv.slice(1) : require(\"system\").args);\n}"
+//    + "\n}"
 
     return out;
   };
