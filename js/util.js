a62f99eed73d8fc56b2a6f858695c5285da0bc79
Chiyu Zhong <1606069+CatTail@users.noreply.github.com>
2013-01-05 17:34:49 +0800

    add DnD

--- js/util.js
+++ js/util.js
@@ -1,24 +1,14 @@
 define(function (require, exports) {
   // bind element with data or function with specified interface
-  exports.addBind = function (element, interface, data) {
-    element.binding ? 0 : element.binding = {};
-    element.binding[interface] ?
-      element.binding[interface].push(data) : element.binding[interface] = [data];
-  };
   exports.setBind = function (element, interface, data) {
     element.binding ? 0 : element.binding = {};
     element.binding[interface] = data;
   };
   exports.getBind = function (element, interface) {
-    return element.binding ? element.binding[interface] : undefined;
+    return element && element.binding && element.binding[interface];
   };
-  // if array, remove exactly data in array
   exports.removeBind = function (element, interface, data) {
-    if (Array.isArray(element.binding[interface])) {
-      element.binding[interface].splice(element.binding[interface].indexOf(data), 1);
-    } else {
       delete element.binding[interface];
-    }
   };
 
   // load all the template string
@@ -43,6 +33,14 @@ define(function (require, exports) {
   };
   exports.templates = templates;
 
+  // Card constructor
+  exports.Card = function (title, data) {
+    var rawCard = exports.templates.card({title: title});
+    var card = $(rawCard);
+    exports.setBind(card, 'data', data);
+    return card;
+  };
+
   // display modal
   exports.modal = function (locals) {
     $('.modal .modal-header .content').html(locals.header);
@@ -50,19 +48,5 @@ define(function (require, exports) {
     $('.modal').modal();
   };
 
-  // generate compiler
-  exports.genCompiler = function (rawGrammar, lex) {
-    var catpreter = require('catpreter/catpreter');
-
-    var grammar = require("catpreter/bnf").parse(rawGrammar);
-    var opt = grammar.options || {};
-    if (lex) grammar.lex = require("catpreter/catlex").parse(lex);
-    if (!opt.moduleType) opt.moduleType = "commonjs";
-    if (!opt.moduleName && name) opt.moduleName =
-        name.replace(/-\w/g, function (match){ return match.charAt(1).toUpperCase(); });
-
-    var generator = new catpreter.Generator(grammar, opt);
-    return generator.generate(opt);
-  };
 
 });
