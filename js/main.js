c84342cb0136b11afc91eada505872fbcc4a3f1a
Chiyu Zhong <1606069+CatTail@users.noreply.github.com>
2013-01-05 10:12:31 +0800

    add data binding

--- js/main.js
+++ js/main.js
@@ -1,21 +1,52 @@
 define(function (require, exports) {
   var util = require('./util');
+  var addBind = util.addBind;
+  var setBind = util.setBind;
+  var getBind = util.getBind;
+  var removeBind = util.removeBind;
 
-  var grammar, lex;
-  $.when(
-    $.ajax({
-      url: '/js/sea-modules/catpreter/src/cmm.y',
-      success: function (data) {
-        grammar = data;
-      }
-    }),
-    $.ajax({
-      url: '/js/sea-modules/catpreter/src/cmm.l',
-      success: function (data) {
-        lex = data;
-      }
-    })
-  ).then(function () {
-//    console.log(util.genCompiler(grammar, lex));
+  // data binding
+  $.ajax({
+    url: '/js/sea-modules/catpreter/src/cmm.y',
+    success: function (grammar) {
+      setBind($('#cmm-grammar')[0], 'data', grammar);
+    }
   });
-})
+  $.ajax({
+    url: '/js/sea-modules/catpreter/src/cmm.l',
+    success: function (lex) {
+      setBind($('#cmm-lex')[0], 'data', lex);
+    }
+  })
+  // draggable
+  $('.card').draggable({
+    snap: '.action',
+    snapMode: 'inner',
+    revert: 'invalid',
+    start: function (event, ui) {
+      if (!getBind(this, 'droppable')) return;
+      removeBind(getBind(this, 'droppable'), 'args', this);
+      removeBind(this, 'droppable');
+    }
+  });
+  // droppable
+  $('.action').droppable({
+    drop: function (event, ui) {
+      var draggable = ui.draggable[0];
+      var droppable = this;
+      addBind(droppable, 'args', draggable);
+      setBind(draggable, 'droppable', droppable);
+    },
+  });
+  $('#toolbox').droppable({});
+
+  // show code detail
+  $('.code-detail').click(function () {
+    $('.modal').modal();
+  });
+
+  // execute
+  $('#execute').click(function () {
+    console.log(getBind($('#cmm-grammar')[0], 'data'));
+  });
+});
--- js/util.js
+++ js/util.js
@@ -1,4 +1,26 @@
 define(function (require, exports) {
+  // bind element with data or function with specified interface
+  exports.addBind = function (element, interface, data) {
+    element.binding ? 0 : element.binding = {};
+    element.binding[interface] ?
+      element.binding[interface].push(data) : element.binding[interface] = [data];
+  };
+  exports.setBind = function (element, interface, data) {
+    element.binding ? 0 : element.binding = {};
+    element.binding[interface] = data;
+  };
+  exports.getBind = function (element, interface) {
+    return element.binding ? element.binding[interface] : undefined;
+  };
+  // if array, remove exactly data in array
+  exports.removeBind = function (element, interface, data) {
+    if (Array.isArray(element.binding[interface])) {
+      element.binding[interface].splice(element.binding[interface].indexOf(data), 1);
+    } else {
+      delete element.binding[interface];
+    }
+  };
+
   // generate compiler
   exports.genCompiler = function (rawGrammar, lex) {
     var catpreter = require('catpreter/catpreter');
@@ -7,7 +29,8 @@ define(function (require, exports) {
     var opt = grammar.options || {};
     if (lex) grammar.lex = require("catpreter/catlex").parse(lex);
     if (!opt.moduleType) opt.moduleType = "commonjs";
-    if (!opt.moduleName && name) opt.moduleName = name.replace(/-\w/g, function (match){ return match.charAt(1).toUpperCase(); });
+    if (!opt.moduleName && name) opt.moduleName =
+        name.replace(/-\w/g, function (match){ return match.charAt(1).toUpperCase(); });
 
     var generator = new catpreter.Generator(grammar, opt);
     return generator.generate(opt);
